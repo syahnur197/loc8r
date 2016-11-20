4dd5210073eee77e0c6d906c3a2f62f618e8ad49
Syahnur197 <Syahnurnizam197@gmail.com>
2016-11-20 21:30:23 +0800

    page 94 sudah

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+/* GET 'home' page */
+module.exports.homelist = function(req, res, next) {
+	res.render('index', { title: 'Home' });
+}
+
+/* GET 'Location Info' page */
+module.exports.locationInfo = function(req, res, next) {
+	res.render('index', { title: 'Location Info' });
+}
+
+/* GET 'Add Review' page */
+module.exports.addReview = function(req, res, next) {
+	res.render('index', { title: 'Add Review' });
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
-/* GET home page. */
-router.get('/', ctrlMain.index);
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+
+/* Location Pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other Pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
