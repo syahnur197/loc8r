80cc8e9c2d9be907b1d0d212d69235ed3a1f0d64
Syahnur197 <Syahnurnizam197@gmail.com>
2016-11-22 07:34:00 +0800

    adding location list, info and review

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,11 +4,11 @@ var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
 /* Location Pages */
-router.get('/', ctrlLocations.homelist);
-router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+	router.get('/', ctrlLocations.homelist);
+	router.get('/location', ctrlLocations.locationInfo);
+	router.get('/location/review/new', ctrlLocations.addReview);
 
-/* Other Pages */
-router.get('/about', ctrlOthers.about);
+	/* Other Pages */
+	router.get('/about', ctrlOthers.about);
 
-module.exports = router;
+	module.exports = router;
